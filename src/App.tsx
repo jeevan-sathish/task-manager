fe4c39cedf98de9a393afb5372b01c8dfc6194f0
Jeevan s <aryajeevan68@gmail.com>
2025-10-07 20:07:34 +0530

    added comonent to app .jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import TodoHeader from './components/TodoHeader'
 
 const App:React.FC = () => {
   return (
-    <div className='text-red-400'>App</div>
+    <div className='text-red-400 w-full h-full '>
+<TodoHeader/>
+    </div>
   )
 }
+
+
 
 
 export default App
---
+++ src/components/TodoHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoHeader = () => {
+  return (
+    <div className='w-full h-15 bg-black flex flex-row justify-center items-center'>TodoHeader</div>
+  )
+}
+
+export default TodoHeader
